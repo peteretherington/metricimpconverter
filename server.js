be69f6cfd41832219667914cd14d3c4d067dae86
Peter Etherington <petherington@freedommobile.ca>
2020-07-19 13:44:47 -0400

    Add helmet no-sniff. Update html head. Prettier formatting

--- server.js
+++ server.js
@@ -1,56 +1,61 @@
-'use strict';
+'use strict'
 
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+// Inject Environment Variables
+require('dotenv').config()
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var expect = require('chai').expect;
-var cors = require('cors');
+const express = require('express')
+const bodyParser = require('body-parser')
+const expect = require('chai').expect
+const cors = require('cors')
+const helmet = require('helmet')
 
-var apiRoutes = require('./routes/api.js');
-var fccTestingRoutes = require('./routes/fcctesting.js');
-var runner = require('./test-runner');
+const apiRoutes = require('./routes/api.js')
+const fccTestingRoutes = require('./routes/fcctesting.js')
+const runner = require('./test-runner')
 
-var app = express();
+const app = express()
 
-app.use('/public', express.static(process.cwd() + '/public'));
+// Sets "X-Content-Type-Options: nosniff".
+app.use(helmet.noSniff())
 
-app.use(cors({ origin: '*' })); //For FCC testing purposes only
+app.use('/public', express.static(process.cwd() + '/public'))
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ origin: '*' })) //For FCC testing purposes only
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //Index page (static HTML)
 app.route('/').get(function (req, res) {
-	res.sendFile(process.cwd() + '/views/index.html');
-});
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
 //For FCC testing purposes
-fccTestingRoutes(app);
+fccTestingRoutes(app)
 
 //Routing for API
-apiRoutes(app);
+apiRoutes(app)
 
 //404 Not Found Middleware
 app.use(function (req, res, next) {
-	res.status(404).type('text').send('Not Found');
-});
+  res.status(404).type('text').send('Not Found')
+})
 
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
-	console.log('Listening on port ' + process.env.PORT);
-	if (process.env.NODE_ENV === 'test') {
-		console.log('Running Tests...');
-		setTimeout(function () {
-			try {
-				runner.run();
-			} catch (e) {
-				var error = e;
-				console.log('Tests are not valid:');
-				console.log(error);
-			}
-		}, 1500);
-	}
-});
+  console.log(`http://localhost:${process.env.PORT}`)
+  if (process.env.NODE_ENV === 'test') {
+    console.log('Running Tests...')
+    setTimeout(function () {
+      try {
+        runner.run()
+      } catch (e) {
+        var error = e
+        console.log('Tests are not valid:')
+        console.log(error)
+      }
+    }, 1500)
+  }
+})
 
-module.exports = app; //for testing
+module.exports = app //for testing
